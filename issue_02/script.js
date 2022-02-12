7e43c20ae6aa533f3cc8ed62e9f8d755755617c5
john-trainer <akr.nk.1110@gmail.com>
2022-02-12 18:45:07 +0900

    refactor: Add wording to a tag.

--- issue_02/script.js
+++ issue_02/script.js
@@ -1,6 +1,7 @@
 const ul = document.getElementById('js-ul');
 const a = document.createElement('a');
 a.href = '1.html';
+a.textContent = 'これです';
 const img = document.createElement('img');
 img.src = 'bookmark.png';
 img.alt = 'ブックマーク';
