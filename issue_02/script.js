f13219c5b642bab4146833690580dc553dcd0353
john-trainer <akr.nk.1110@gmail.com>
2022-02-12 18:41:16 +0900

    feat: Creating an img tag.
    Create an img tag and specify the src and alt.

--- issue_02/script.js
+++ issue_02/script.js
@@ -1,3 +1,6 @@
 const ul = document.getElementById('js-ul');
 const a = document.createElement('a');
 a.href = '1.html';
+const img = document.createElement('img');
+img.src = 'bookmark.png';
+img.alt = 'ブックマーク';
