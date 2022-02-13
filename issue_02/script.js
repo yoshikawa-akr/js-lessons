7153bf1f4d316bb3963d19b4b50d414311a7a8b6
john-trainer <akr.nk.1110@gmail.com>
2022-02-13 11:01:42 +0900

    refactor: Improved readability
    The readability was improved by writing the constants and their processing descriptions separately.

--- issue_02/script.js
+++ issue_02/script.js
@@ -1,10 +1,11 @@
 const ul = document.getElementById('js-ul');
+const li = document.createElement('li');
 const a = document.createElement('a');
-a.href = '1.html';
-a.textContent = 'これです';
 const img = document.createElement('img');
+
 img.src = 'bookmark.png';
 img.alt = 'ブックマーク';
+a.href = '1.html';
+a.textContent = 'これです';
 a.insertAdjacentElement('afterbegin', img);
-const li = document.createElement('li');
 ul.appendChild(li).appendChild(a);
