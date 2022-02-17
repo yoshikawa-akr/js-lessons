816708d255f9bfe3f5f72d12ac7e95a7cdc96408
john-trainer <akr.nk.1110@gmail.com>
2022-02-17 15:48:49 +0900

    Add new attributes and text.

--- issue_03/script.js
+++ issue_03/script.js
@@ -3,3 +3,7 @@ const ul = document.getElementById('js-ul');
 const li = document.createElement('li');
 const a = document.createElement('a');
 const img = document.createElement('img');
+
+a.setAttribute('href', 'a1.html');
+a.textContent = 'a1';
+img.setAttribute('src', '/img/bookmark.png');
