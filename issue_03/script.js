f8861eb7ea7541bd2ce9e6fd51819d2982ec681c
john-trainer <akr.nk.1110@gmail.com>
2022-02-17 18:24:05 +0900

    style: Delete blank lines.

--- issue_03/script.js
+++ issue_03/script.js
@@ -1,5 +1,4 @@
 const ul = document.getElementById('js-ul');
-
 const li = document.createElement('li');
 const a = document.createElement('a');
 const img = document.createElement('img');
@@ -7,7 +6,6 @@ const img = document.createElement('img');
 a.setAttribute('href', 'a1.html');
 a.textContent = 'a1';
 img.setAttribute('src', '/img/bookmark.png');
-
 a.insertAdjacentElement('afterbegin', img);
 ul.appendChild(li).appendChild(a);
 
