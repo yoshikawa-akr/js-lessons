72292752edebc6e9a70f73cb5cfd8c14c8b8ab0e
john-trainer <akr.nk.1110@gmail.com>
2022-02-21 19:29:51 +0900

    feat: Create the specified array.

---
+++ issue_04/script.js
@@ -0,0 +1,11 @@
+const contentsArray = [{
+    to: "bookmark.html",
+    img: "1.png",
+    alt: "画像1",
+    text: "ブックマーク"
+}, {
+    to: "message.html",
+    img: "2.png",
+    alt: "画像2",
+    text: "メッセージ"
+}];
