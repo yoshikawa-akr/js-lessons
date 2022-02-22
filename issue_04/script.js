09e6c6c3474b8d48070835e761e7be13657cc2cf
john-trainer <akr.nk.1110@gmail.com>
2022-02-22 18:47:31 +0900

    feat: Add processing for issue 4.

--- issue_04/script.js
+++ issue_04/script.js
@@ -9,3 +9,20 @@ const contentsArray = [{
     alt: "画像2",
     text: "メッセージ"
 }];
+
+const ul = document.createElement('ul');
+const fragment = document.createDocumentFragment();
+
+for (const item of contentsArray) {
+    const li = document.createElement('li');
+    const a = document.createElement('a');
+    const img = document.createElement('img');
+
+    a.textContent = item.text;
+    a.href = item.to;
+    img.src = item.img;
+    img.alt = item.alt;
+
+    fragment.appendChild(li).appendChild(a).insertAdjacentElement('afterbegin', img);
+}
+document.body.insertAdjacentElement('afterbegin', ul).appendChild(fragment);
