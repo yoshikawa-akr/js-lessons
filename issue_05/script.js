72d5781b2119f6b35239766810609cb5942b5579
john-trainer <akr.nk.1110@gmail.com>
2022-02-25 20:03:26 +0900

    feat: Add a process using promise.

--- issue_05/script.js
+++ issue_05/script.js
@@ -9,3 +9,22 @@ const contents = [{
     alt: "画像2",
     text: "メッセージ"
 }];
+
+const ul = document.getElementById('js-ul');
+const promise = new Promise((resolve) => {
+    resolve(contents);
+}).then((value) => {
+    const fragment = document.createDocumentFragment();
+    for (let i = 0; i < value.length; i++) {
+        const li = document.createElement('li');
+        const a = document.createElement('a');
+        const img = document.createElement('img');
+
+        a.textContent = value[i].text;
+        a.href = `/${value[i].to}`;
+        img.src = value[i].img;
+        img.alt = value[i].alt;
+        fragment.appendChild(li).appendChild(a).insertAdjacentElement('afterbegin', img);
+    }
+    ul.appendChild(fragment);
+});
