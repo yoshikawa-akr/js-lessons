e30ac5dfb5eeacbb03de9f218ff6c567b75b32b8
john-trainer <akr.nk.1110@gmail.com>
2022-03-06 14:49:50 +0900

    feat: Create createLists function.

--- issue_08/script.js
+++ issue_08/script.js
@@ -9,3 +9,28 @@ function startLoading() {
 function stopLoading() {
     document.getElementById('js-loading').remove(loadingImg);
 }
+
+function createLists() {
+    const ul = document.getElementById('js-ul');
+    const fragment = document.createDocumentFragment();
+    const listData = [{
+        img: "1.png",
+        alt: "画像1",
+        text: "ブックマーク"
+    }, {
+        img: "2.png",
+        alt: "画像2",
+        text: "メッセージ"
+    }];
+
+    for (const item of listData) {
+        const li = document.createElement('li');
+        const img = document.createElement('img');
+
+        li.textContent = item.text;
+        img.src = item.img;
+        img.alt = item.alt;
+        fragment.appendChild(li).appendChild(img);
+    }
+    ul.appendChild(fragment);
+}
