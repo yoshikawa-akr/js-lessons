0d3f07f1cab0a4d075fdbd82c2cbe7c364c7864b
john-trainer <akr.nk.1110@gmail.com>
2022-03-06 18:10:57 +0900

    feat: Create getListData.
    Create a promise object and describe the process when it is rejected.

--- issue_08/script.js
+++ issue_08/script.js
@@ -10,20 +10,11 @@ function stopLoading() {
     document.getElementById('js-loading').remove(loadingImg);
 }
 
-function createLists() {
+function createLists(resolvedData) {
     const ul = document.getElementById('js-ul');
     const fragment = document.createDocumentFragment();
-    const listData = [{
-        img: "1.png",
-        alt: "画像1",
-        text: "ブックマーク"
-    }, {
-        img: "2.png",
-        alt: "画像2",
-        text: "メッセージ"
-    }];
 
-    for (const item of listData) {
+    for (const item of resolvedData) {
         const li = document.createElement('li');
         const img = document.createElement('img');
 
@@ -34,3 +25,25 @@ function createLists() {
     }
     ul.appendChild(fragment);
 }
+
+const getListData = new Promise((reject) => {
+    startLoading();
+    const listData = [{
+        img: "1.png",
+        alt: "画像1",
+        text: "ブックマーク"
+    }, {
+        img: "2.png",
+        alt: "画像2",
+        text: "メッセージ"
+    }];
+    setTimeout(() => {
+        reject(Error('エラーです'));
+    }, 3000);
+});
+getListData.catch((error) => {
+    return error;
+});
+getListData.then((catched) => {
+    console.error(catched);
+});
