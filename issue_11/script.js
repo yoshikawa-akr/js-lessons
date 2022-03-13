12aa46f5fbeba1b7e0ff41fc476733b5d0af4850
john-trainer <akr.nk.1110@gmail.com>
2022-03-13 19:24:48 +0900

    feat: Asynchronous function addition.
    Obtain a json object and display it as a list in the DOM.

---
+++ issue_11/script.js
@@ -0,0 +1,61 @@
+function startLoading() {
+    const loadingImg = document.createElement('img');
+    loadingImg.src = 'loading-circle.gif';
+    loadingImg.alt = 'ローディング画像';
+    document.getElementById('js-loading').appendChild(loadingImg);
+}
+
+function stopLoading() {
+    document.getElementById('js-loading').remove();
+}
+
+function createLists(resolvedData) {
+    const ul = document.getElementById('js-ul');
+    const fragment = document.createDocumentFragment();
+    for (const item of resolvedData) {
+        const li = document.createElement('li');
+        const a = document.createElement('a');
+        const img = document.createElement('img');
+
+        a.href = item.to;
+        a.textContent = item.text;
+        img.src = item.img;
+        img.alt = item.alt;
+
+        fragment.appendChild(li).appendChild(a).appendChild(img);
+    }
+    ul.appendChild(fragment);
+}
+
+
+async function callJson() {
+    const url = 'https://api.json-generator.com/templates/NluaaELSLhVe/data?access_token=ambjpfxjl00e50wa639kwndq1ofq3iuykdrv98ge';
+    const response = await fetch(url);
+    const result = await response.json();
+    return result.data;
+}
+
+function getJsonData() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(callJson());
+        }, 3000);
+    });
+}
+
+async function tryGetJsonData() {
+    startLoading();
+    try {
+        return await getJsonData();
+    } catch (error) {
+        console.error(error);
+    } finally {
+        stopLoading();
+    }
+}
+
+async function showList() {
+    const result = await tryGetJsonData();
+    createLists(result);
+}
+showList();
