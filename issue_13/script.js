7372a5ae6599ec3741ccf329a0690710becf8849
john-trainer <akr.nk.1110@gmail.com>
2022-03-31 19:45:00 +0900

    feat: Implementation of functions
    Once implemented to the point where it works according to specifications.

---
+++ issue_13/script.js
@@ -0,0 +1,84 @@
+const modalBtnWrapper = document.getElementById('js-modal-btn__wrapper');
+const modalBtn = document.getElementById('js-modal-btn');
+const modal = document.getElementById('js-modal');
+const requestBtn = document.getElementById('js-request-btn');
+const closeBtn = document.getElementById('js-close-btn');
+
+modalBtn.addEventListener('click', () => {
+    modalBtnWrapper.classList.add('hide');
+    modal.classList.add('active');
+});
+
+closeBtn.addEventListener('click', () => {
+    modalBtnWrapper.classList.remove('hide')
+    modal.classList.remove('active');
+});
+requestBtn.addEventListener('click', () => {
+    init();
+    modal.remove();
+});
+
+function startLoading() {
+    const loadingImg = document.createElement('img');
+    loadingImg.src = 'loading-circle.gif';
+    loadingImg.alt = 'ローディング画像';
+    document.getElementById('js-loading').appendChild(loadingImg);
+}
+
+function removeLoading() {
+    document.getElementById('js-loading').remove();
+}
+
+function renderList(resolvedData) {
+    const ul = document.getElementById('js-ul');
+    const fragment = document.createDocumentFragment();
+    for (const item of resolvedData) {
+        const li = document.createElement('li');
+        const a = document.createElement('a');
+        const img = document.createElement('img');
+
+        a.href = item.a;
+        a.textContent = item.text;
+        img.src = item.img;
+        img.alt = item.alt;
+
+        fragment.appendChild(li).appendChild(a).appendChild(img);
+    }
+    ul.appendChild(fragment);
+}
+
+function renderErrorMessage(messageText) {
+    document.getElementById('js-ul').textContent = messageText;
+}
+
+async function fetchData() {
+    try {
+        const url = 'https://api.json-generator.com/templates/NluaaELSLhVe/data?access_token=ambjpfxjl00e50wa639kwndq1ofq3iuykdrv98ge';
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`${response.status}:${response.statusText}`);
+        }
+        const result = await response.json();
+        return result.data;
+    } catch (e) {
+        throw new Error(e);
+    }
+}
+
+async function init() {
+    startLoading();
+    let listData;
+    try {
+        listData = await fetchData();
+    } catch (e) {
+        console.error(e.message);
+        renderErrorMessage(e.message);
+    } finally {
+        removeLoading();
+    }
+    if (listData.length === 0) {
+        console.log('データがありません');
+        renderErrorMessage('データがありません');
+    }
+    renderList(listData);
+}
